8c29556865f2f1f0ed37ac1f6199195d73ef9c7f
gawmk <mikolaj.gawrys@gmail.com>
2026-01-25 16:21:57 +0100

    dwmblocks init

--- config.h
+++ config.h
@@ -2,13 +2,13 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER " | "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
 
 // Control whether blocks are clickable.
-#define CLICKABLE_BLOCKS 1
+#define CLICKABLE_BLOCKS 0
 
 // Control whether a leading delimiter should be prepended to the status.
 #define LEADING_DELIMITER 0
@@ -18,15 +18,10 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("", "sb-mail", 600, 1)  \
-    X("", "sb-music", 0, 2)   \
-    X("", "sb-disk", 1800, 3) \
-    X("", "sb-memory", 10, 4) \
-    X("", "sb-loadavg", 5, 5) \
-    X("", "sb-mic", 0, 6)     \
-    X("", "sb-record", 0, 7)  \
-    X("", "sb-volume", 0, 8)  \
-    X("", "sb-battery", 5, 9) \
-    X("", "sb-date", 1, 10)
+    X("", "/home/gawmk/dotfiles/scripts/x11/stats", 5, 0) \
+    X("", "/home/gawmk/dotfiles/scripts/x11/audio-status", 0, 1) \
+    X("", "/home/gawmk/dotfiles/scripts/x11/network-status", 5, 0) \
+    X("", "/home/gawmk/dotfiles/scripts/x11/battery-status", 60, 0) \
+    X("", "date \"+%_k:%M  %a %b %d\" ", 60, 0)
 
 #endif  // CONFIG_H
